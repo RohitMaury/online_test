a1dc91615f0bc839cf3c54e05b7f6cba09f2e003
ROHIT MAURYA <rockymaurya13999@gmail.com>
2023-03-24 15:55:51 +0530

    commit

---
+++ project.sql
@@ -0,0 +1,317 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 03, 2020 at 04:47 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--xcz 
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) 
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `email`, `password`) VALUES
+(2, 'admin@gmail.com', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+CREATE TABLE `answer` (
+  `qid` text NOT NULL,
+  `ansid` text NOT NULL
+) 
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`qid`, `ansid`) VALUES
+('558922117fcef', '5589221195248'),
+('55892211e44d5', '55892211f1fa7'),
+('558922894c453', '558922895ea0a'),
+('558922899ccaa', '55892289aa7cf'),
+('558923538f48d', '558923539a46c'),
+('55892353f05c4', '55892354051be');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` text NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(500) NOT NULL,
+  `feedback` varchar(500) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(50) NOT NULL
+) 
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `feedback`, `date`, `time`) VALUES
+('55846be776610', 'testing', 'sunnygkp10@gmail.com', 'testing', 'testing stART', '2015-06-19', '09:22:15pm'),
+('5584ddd0da0ab', 'netcamp', 'sunnygkp10@gmail.com', 'feedback', ';mLBLB', '2015-06-20', '05:28:16am'),
+('558510a8a1234', 'sunnygkp10', 'sunnygkp10@gmail.com', 'dl;dsnklfn', 'fmdsfld fdj', '2015-06-20', '09:05:12am'),
+('5585509097ae2', 'sunny', 'sunnygkp10@gmail.com', 'kcsncsk', 'l.mdsavn', '2015-06-20', '01:37:52pm'),
+('5586ee27af2c9', 'vikas', 'vikas@gmail.com', 'trial feedback', 'triaal feedbak', '2015-06-21', '07:02:31pm'),
+('5589858b6c43b', 'nik', 'nik1@gmail.com', 'good', 'good site', '2015-06-23', '06:12:59pm'),
+('5ed8ce52b8426', 'vishnu', 'vishnu@gmail.com', 'kesa laga', 'bjkhjhjhjk', '2020-06-04', '12:34:58pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history` (
+  `email` varchar(50) NOT NULL,
+  `eid` text NOT NULL,
+  `score` int(11) NOT NULL,
+  `level` int(11) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) 
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
+('sunnygkp10@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 09:31:26'),
+('avantika420@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 14:33:04'),
+('avantika420@gmail.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 14:49:39'),
+('mi5@hollywood.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 15:12:56'),
+('nik1@gmail.com', '558921841f1ec', 1, 2, 1, 1, '2015-06-23 16:11:50'),
+('sunnygkp10@gmail.com', '5589222f16b93', 1, 2, 1, 1, '2015-06-24 03:22:38'),
+('hkgammer6@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2020-10-25 07:34:30'),
+('hkgammer6@gmail.com', '5589222f16b93', -2, 2, 0, 2, '2020-10-25 08:19:16'),
+('hkgammer6@gmail.com', '558922ec03021', -2, 2, 0, 2, '2020-10-25 08:23:04'),
+('vk74714@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2020-11-03 03:46:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `qid` varchar(50) NOT NULL,
+  `option` varchar(5000) NOT NULL,
+  `optionid` text NOT NULL
+) 
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
+('558922117fcef', 'echo', '5589221195248'),
+('558922117fcef', 'print', '558922119525a'),
+('558922117fcef', 'printf', '5589221195265'),
+('558922117fcef', 'cout', '5589221195270'),
+('55892211e44d5', 'int a', '55892211f1f97'),
+('55892211e44d5', '$a', '55892211f1fa7'),
+('55892211e44d5', 'long int a', '55892211f1fb4'),
+('55892211e44d5', 'int a$', '55892211f1fbd'),
+('558922894c453', 'cin>>a;', '558922895ea0a'),
+('558922894c453', 'cin<<a;', '558922895ea26'),
+('558922894c453', 'cout>>a;', '558922895ea34'),
+('558922894c453', 'cout<a;', '558922895ea41'),
+('558922899ccaa', 'cout', '55892289aa7cf'),
+('558922899ccaa', 'cin', '55892289aa7df'),
+('558922899ccaa', 'print', '55892289aa7eb'),
+('558922899ccaa', 'printf', '55892289aa7f5'),
+('558923538f48d', '255.0.0.0', '558923539a46c'),
+('558923538f48d', '255.255.255.0', '558923539a480'),
+('558923538f48d', '255.255.0.0', '558923539a48b'),
+('558923538f48d', 'none of these', '558923539a495'),
+('55892353f05c4', '192.168.1.100', '5589235405192'),
+('55892353f05c4', '172.168.16.2', '55892354051a3'),
+('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
+('55892353f05c4', '11.11.11.11', '55892354051be');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `eid` text NOT NULL,
+  `qid` text NOT NULL,
+  `qns` text NOT NULL,
+  `choice` int(10) NOT NULL,
+  `sn` int(11) NOT NULL
+)
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
+('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
+('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
+('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
+('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
+('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
+('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `eid` text NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `sahi` int(11) NOT NULL,
+  `wrong` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `time` bigint(20) NOT NULL,
+  `intro` text NOT NULL,
+  `tag` varchar(100) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+)
+
+--
+-- Dumping data for table `quiz`
+--
+
+INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
+('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2015-06-23 09:06:12'),
+('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2015-06-23 09:09:03'),
+('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2015-06-23 09:12:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rank`
+--
+
+CREATE TABLE `rank` (
+  `email` varchar(50) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+)
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`email`, `score`, `time`) VALUES
+('sunnygkp10@gmail.com', 9, '2015-06-24 03:22:38'),
+('avantika420@gmail.com', 8, '2015-06-23 14:49:39'),
+('mi5@hollywood.com', 4, '2015-06-23 15:12:56'),
+('nik1@gmail.com', 1, '2015-06-23 16:11:50'),
+('vk74717@gjhkjk.jh', -1, '2020-06-03 18:18:08'),
+('hjggQghvhv@hjbhjv.bshhj', -2, '2020-06-03 18:20:48'),
+('suhana@gmail.com', -4, '2020-06-04 10:04:48'),
+('bhabi@gmail.com', 0, '2020-06-04 10:10:58'),
+('hkgammer6@gmail.com', 0, '2020-10-25 08:23:05'),
+('vk74714@gmail.com', 4, '2020-11-03 03:46:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(5) NOT NULL,
+  `college` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mob` bigint(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) 
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Avantika', 'F', 'KNIT sultanpur', 'avantika420@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('Bhabi', 'F', 'hjkhhq', 'bhabi@gmail.com', 55457865, '83da74ed099a806f7b0a12ee528c8dda'),
+('Mark Zukarburg', 'M', 'Stanford', 'ceo@facebook.com', 987654321, 'e10adc3949ba59abbe56e057f20f883e'),
+('Vishnu', 'F', 'gli', 'hjggQghvhv@hjbhjv.bshhj', 85665662566, '1963fd70e789022f6f5b11498f992404'),
+('Vishnu Sharma', 'M', 'xz', 'hkgammer6@gmail.com', 0, '01aeb8ef99b2576aa4093c08c3f04325'),
+('Komal', 'F', 'KNIT sultanpur', 'komalpd2011@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('Tom Cruze', 'M', 'Hollywood', 'mi5@hollywood.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('Netcamp', 'M', 'KNIT sultanpur', 'netcamp@gmail.com', 987654321, 'e10adc3949ba59abbe56e057f20f883e'),
+('Nikunj', 'M', 'XYZ', 'nik1@gmail.com', 987, '202cb962ac59075b964b07152d234b70'),
+('Somu', 'M', 'SOMU', 'SOMU@GMAIL.COM', 4545445454545454, 'somu@123'),
+('Suhana', 'M', 'hjhjh', 'suhana@gmail.com', 54777545454, '7b297bb3239c6bcb276d78e38f5b463c'),
+('Sunny', 'M', 'KNIT sultanpur', 'sunnygkp10@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('User', 'M', 'cimt', 'user@user.com', 11, 'e10adc3949ba59abbe56e057f20f883e'),
+('Vikash', 'M', 'KNIT sultanpur@gmail.com', 'vikash@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
+('Vishnu Sharma', 'M', 'gli', 'vk74714@gmail.com', 7055528608, 'e3afed0047b08059d0fada10f400c1e5'),
+('Vishnu', 'M', 'gli', 'vk74717@gjhkjk.jh', 7451860948, '1963fd70e789022f6f5b11498f992404');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
